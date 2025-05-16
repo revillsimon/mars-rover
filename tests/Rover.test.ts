cdd7c1e24b81347e0174bb47bd454b24426278f9
Simon Revill <simon.revill@vertumotors.com>
2025-05-16 13:08:52 +0100

    rover: should have a heading of 'W' when turning 'R' from a heading of 'S'

--- tests/Rover.test.ts
+++ tests/Rover.test.ts
@@ -10,6 +10,7 @@ describe("Rover tests", () => {
       { turnDirection: "L", currentHeading: "E", expectedHeading: "N" },
       { turnDirection: "R", currentHeading: "N", expectedHeading: "E" },
       { turnDirection: "R", currentHeading: "E", expectedHeading: "S" },
+      { turnDirection: "R", currentHeading: "S", expectedHeading: "W" },
     ])(
       "should have a heading of $expectedHeading when turning $turnDirection from a heading of $currentHeading",
       ({ turnDirection, currentHeading, expectedHeading }) => {
