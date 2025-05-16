2de156cb1376309f99b9a84032db3d2206752e72
Simon Revill <simon.revill@vertumotors.com>
2025-05-16 17:39:58 +0100

    refactor: move grid coordinate parsing to instance method

--- src/MissionControl.ts
+++ src/MissionControl.ts
@@ -4,11 +4,14 @@ export class MissionControl {
   private _upperRightPlateauCoordinates: { x: number; y: number };
 
   constructor({ grid }: MissionControlConfig) {
-    const [x, y] = grid
-      .split(" ")
-      .map((numberString) => parseInt(numberString));
+    const [x, y] = this.parseGridCoordinates(grid);
 
     this._upperRightPlateauCoordinates = { x, y };
+  }
+
+  private parseGridCoordinates(input: string): [number, number] {
+    const [x, y] = input.split(" ").map((number) => parseInt(number));
+    return [x, y];
   }
 
   public get upperRightPlateauCoordinates(): { x: number; y: number } {
